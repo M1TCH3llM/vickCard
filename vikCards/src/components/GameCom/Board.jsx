bf9cbbb471da55ac02ee69d7a8f5b6e469ce8625
Mitchell.Morgan <mitchemorgan@gmail.com>
2024-01-18 18:32:06 -0700

    added cat cards

---
+++ vikCards/src/components/GameCom/Board.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./board.css";
+
+function Board() {
+  return (
+    <div id="boardCol">
+      <div>Card Place Holder</div>
+      <div>You are</div>
+      <div>You are not</div>
+      <div>You are torn</div>
+      <div>You are does not apply</div>
+    </div>
+  );
+}
+
+export default Board;
--- vikCards/src/pages/game.jsx
+++ vikCards/src/pages/game.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import Nav from '../components/GameCom/nav'
+import React from "react";
+import Nav from "../components/GameCom/nav";
+import Board from "../components/GameCom/Board";
 
 function game() {
   return (
     <div>
       <Nav />
+      <Board />
     </div>
-  )
+  );
 }
 
-export default game
+export default game;
